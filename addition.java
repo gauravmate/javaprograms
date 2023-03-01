69172d1084639d113f374a94d374ea702e781d48
gauravmate <gmate7292@gmail.com>
2023-03-01 10:00:42 +0530

    demo.java

---
+++ addition.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class addition {
+    public static void main(String [] args) 
+    {   
+        System.out.println("Enter two number");
+       Scanner sc=new Scanner(System.in);
+       int a=sc.nextInt();
+       int b=sc.nextInt();
+       int sum=0;
+       System.out.print(sum=a+b);
+    }
+    
+}
+
+
+                                        
